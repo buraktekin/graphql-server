57697fdd7604e808562b0d1c4e9ae0b7000164c6
buraktekin <tknbrk@gmail.com>
2020-05-07 07:03:51 +0200

    🔺 add search option

--- gql/sentences/schema.py
+++ gql/sentences/schema.py
@@ -1,5 +1,6 @@
 import graphene
 from graphene_django import DjangoObjectType
+from django.db.models import Q
 
 from .models import Sentence
 
@@ -10,7 +11,12 @@ class SentenceType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    sentences = graphene.List(SentenceType)
+    sentences = graphene.List(SentenceType, search=graphene.String())
 
-    def resolve_sentences(self, info, **kwargs):
+    def resolve_sentences(self, info, search=None, **kwargs):
+        if search:
+            filter = (
+                Q(id__icontains=search)
+            )
+            return Sentence.objects.filter(filter)
         return Sentence.objects.all()
